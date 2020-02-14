66987150bda7574a691ef25b2eefcfeb34459a34
Phi Tran <ptran@securitycompass.com>
2020-02-14 18:11:19 -0500

    Start app_mention handler

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
+import slack
 from slackeventsapi import SlackEventAdapter
 
 from settings import *
 
-
+client = slack.WebClient(token=SLACK_BOT_TOKEN)
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
 
 
-# Create an event listener for "reaction_added" events and print the emoji name
 @slack_events_adapter.on("reaction_added")
 def reaction_added(event_data):
     emoji = event_data["event"]["reaction"]
     print(emoji)
+
+
+@slack_events_adapter.on("app_mention")
+def pick_volunteer(event_data):
+    text = event_data["text"]
+    user = event_data["user"]
+    channel = event_data["channel"]
+    response_text = "I choose you"
+    client.chat_postMessage(channel=channel, text=response_text)
 
 
 # Start the server
